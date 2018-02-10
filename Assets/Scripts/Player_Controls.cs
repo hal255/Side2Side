902489b275ae69c301c39486e58a902b42d01480
Hong Luu <hluu114@gmail.com>
2018-02-10 15:15:06 -0800

    Added jumping abilities, need to fix jumping bugs

--- Assets/Scripts/Player_Controls.cs
+++ Assets/Scripts/Player_Controls.cs
@@ -9,11 +9,16 @@ public class Player_Controls : MonoBehaviour {
     public bool move_right = false;
     public bool move_up = false;
     public bool move_down = false;
-    public bool is_jumping = false;
 
     public float x_pos = 0.0f;
     public float y_pos = 0.0f;
     public float movement_speed = 5.0f;
+
+    // jumping variables
+    public bool is_jumping = false;
+    public bool is_falling = true;
+    public float jump_y = 0.0f;
+    public float max_ground_distance;
 
     // Use this for initialization
     void Start () {
@@ -23,7 +28,8 @@ public class Player_Controls : MonoBehaviour {
             Camera cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
             setCamera(cam);
         }
-	}
+        max_ground_distance = 0.05f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -41,12 +47,32 @@ public class Player_Controls : MonoBehaviour {
 
         // jump up
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             is_jumping = true;
+            is_falling = true;
+        }
         if (Input.GetKeyUp(KeyCode.Space))
             is_jumping = false;
 
+        //checkInAir();
         moveLeft();
         moveRight();
+        jumpUp();
+    }
+
+    // handles when character lands on something
+    private void FixedUpdate()
+    {
+        if (is_falling)
+        {
+            Vector2 feet_pos = new Vector2(transform.position.x, transform.position.y - 0.98f);
+            RaycastHit2D hit = Physics2D.Raycast(feet_pos, Vector2.down, max_ground_distance);
+            if (hit)
+            {
+                Debug.Log("Hit: " + hit.collider.name + ", distance: " + hit.distance);
+                is_falling = false;
+            }
+        }
 
     }
 
@@ -78,6 +104,19 @@ public class Player_Controls : MonoBehaviour {
     public void moveDown()
     {
 
+    }
+
+    public void jumpUp()
+    {
+        // adjust gravity if jumping
+        if (is_jumping && !is_falling)
+        {
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = -30;
+            is_jumping = false;
+        }
+        // else, let gravity handle the falling
+        else
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
     }
 
     public void setCamera(Camera cam)
